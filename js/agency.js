6e64acf012239517915f9d1fc186eabbb52d766a
CarolSum <905965287@qq.com>
2017-11-11 16:22:21 +0800

    initial commit

---
+++ js/agency.js
@@ -0,0 +1,12 @@
+
+//jquery easing 插件
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
